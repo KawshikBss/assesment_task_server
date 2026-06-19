23a88bcb4ee520e9570cdd133a8fd3efa53f8827
KawshikBss <kawshikbss@gmail.com>
2026-06-19 12:42:42 +0600

    added auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('/test', function (Request $request) {
     return response()->json('test');
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
