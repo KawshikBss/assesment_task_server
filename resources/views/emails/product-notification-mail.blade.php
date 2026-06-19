d0990415278540cb12d36e215cbafbf1e227395b
KawshikBss <kawshikbss@gmail.com>
2026-06-19 18:04:20 +0600

    modified

--- resources/views/emails/product-notification-mail.blade.php
+++ resources/views/emails/product-notification-mail.blade.php
@@ -1,22 +1,23 @@
 <h1>Product {{ $action }}</h1>
 
-<p>Your product has been <strong>{{ $action }}</strong>.</p>
+<p>Greetings, {{ $product->user->name }}.</p>
+<p>Your product has been {{ $action }}.</p>
 
 <table>
     <tr>
-        <th>Name</th>
+        <th align="left">Name</th>
         <td>{{ $product->name }}</td>
     </tr>
     <tr>
-        <th>Description</th>
+        <th align="left">Description</th>
         <td>{{ $product->description }}</td>
     </tr>
     <tr>
-        <th>Category</th>
+        <th align="left">Category</th>
         <td>{{ $product->category->name }}</td>
     </tr>
     <tr>
-        <th>Price</th>
+        <th align="left">Price</th>
         <td>{{ $product->price }}</td>
     </tr>
 </table>
