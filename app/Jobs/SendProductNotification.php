52b72143bce6a84c22fa2a512d8f369983956706
KawshikBss <kawshikbss@gmail.com>
2026-06-19 17:22:01 +0600

    mofified job to send mail

--- app/Jobs/SendProductNotification.php
+++ app/Jobs/SendProductNotification.php
@@ -2,19 +2,27 @@
 
 namespace App\Jobs;
 
+use App\Mail\ProductNotificationMail;
+use App\Models\Product;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class SendProductNotification implements ShouldQueue
 {
     use Queueable;
 
+    public Product $product;
+    public string $action;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(Product $product, string $action)
     {
-        //
+        $this->product = $product;
+        $this->action = $action;
     }
 
     /**
@@ -22,6 +30,7 @@ class SendProductNotification implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $this->product->loadMissing('category');
+        Mail::to('kawshikbss@gmail.com')->send(new ProductNotificationMail($this->product, $this->action));
     }
 }
